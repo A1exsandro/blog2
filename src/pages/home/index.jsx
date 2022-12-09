be7e158d0c6252c65a56a03f8a4eadf3821f911e
A1exsandro <alexsandro.acp@gmail.com>
2022-12-08 20:51:48 -0400

    add about and home pages

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import FeaturedPost from "../../components/featured-post";
+import PostList from "../../components/post-list";
+
+export default function Home(){
+	return(
+		<>
+			<FeaturedPost />
+			<PostList />
+		</>
+	)
+}
