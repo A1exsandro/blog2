708debb00c94ec23a46dc9d9cbac75ab62871f8b
A1exsandro <alexsandro.acp@gmail.com>
2022-12-08 20:53:26 -0400

    changes in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,17 @@ import {
     Routes
 } from "react-router-dom";
 
+import Home from "../pages/home";
+import About from "../pages/about";
+import NavBar from "../components/navbar";
+
 export default function AppRoutes(){
   return(
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<h1>it is work</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
