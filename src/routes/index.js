1c79b0834fff6b475f3651ec6544957509d1b8b1
A1exsandro <alexsandro.acp@gmail.com>
2022-12-28 14:19:22 -0400

    changes in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import {
 import Home from "../pages/home";
 import About from "../pages/about";
 import NavBar from "../components/navbar";
+import English from "../pages/english";
 
 export default function AppRoutes(){
   return(
@@ -15,6 +16,7 @@ export default function AppRoutes(){
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/english" element={<English />} />
       </Routes>
     </BrowserRouter>
   )
