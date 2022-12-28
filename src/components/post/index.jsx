be80c8039468f983ea4807e243229d666f4473e7
A1exsandro <alexsandro.acp@gmail.com>
2022-12-28 13:48:01 -0400

    add link in post

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,22 +1,25 @@
+import { Link } from "react-router-dom"
 import AuthorBadge from "../author-badge"
 import "./styles.css"
 
 export default function Post({ post }){
   return(
-    <div className="card">
-      <div>
-        <img className="imgContainer" src={post.image} />
-      </div>
+    <Link to="/about"> 
+      <div className="card">
+        <div>
+          <img className="imgContainer" src={post.image} />
+        </div>
 
-      <div className="text">
-        <h2>First Title</h2>
-        <div className="details">
-          <AuthorBadge />
-          <div className="date">
-            <h3>17 de Janeiro</h3>
+        <div className="text">
+          <h2>First Title</h2>
+          <div className="details">
+            <AuthorBadge />
+            <div className="date">
+              <h3>17 de Janeiro</h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
