3734655fa06ed14bef875f8e01b040234c46fd04
A1exsandro <alexsandro.acp@gmail.com>
2023-01-09 07:35:43 -0400

    contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,7 +1,34 @@
 import "./styles.css"
 
 export default function Contact(){
-    return(
-        <div className="containerContact">Contact</div>
-    )
+  return(
+    <div className="containerContact">
+      <div>Contact</div> 
+
+      <div className="contactCard">
+        <div className="contactAvatar">Avatar</div>
+        <div className="contactName">Nome</div>
+      </div>
+
+      <div className="contactCard">
+        <div className="contactAvatar">Avatar</div>
+        <div className="contactName">Alexsandro</div>
+      </div>
+
+      <div className="contactCard">
+        <div className="contactAvatar">Avatar</div>
+        <div className="contactName">Antônio</div>
+      </div>
+
+      <div className="contactCard">
+        <div className="contactAvatar">Avatar</div>
+        <div className="contactName">Adriana</div>
+      </div>
+
+      <div className="contactCard">
+        <div className="contactAvatar">Avatar</div>
+        <div className="contactName">Lucas</div>
+      </div>
+    </div>
+  )
 }
