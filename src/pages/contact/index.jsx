2493fa628f4cf405d43c1ed20db127543d785c99
A1exsandro <alexsandro.acp@gmail.com>
2023-01-13 15:00:55 -0400

    test in contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -4,35 +4,47 @@ const students = [
   {
     id: 1,
     name: "Alexsando",
-    shift: "noturno"
+    shift: "noturno",
+    presence: "false"
   },
   {
     id: 2,
     name: "Anderson",
-    shift: "noturno"
+    shift: "noturno",
+    presence: "false"
   },
   {
     id: 3,
     name: "Antônio",
-    shift: "noturno"
+    shift: "noturno",
+    presence: "false"
   },
   {
     id: 4,
     name: "Alan",
-    shift: "noturno"
+    shift: "noturno",
+    presence: "false"
   },
   {
     id: 5,
     name: "Alex",
-    shift: "noturno"
+    shift: "noturno",
+    presence: "false"
   },
   {
     id: 6,
     name: "Augusto",
-    shift: "noturno"
+    shift: "noturno",
+    presence: "true"
   },
 ]
-
+const calls = [
+  {
+    date: "12/01/2023",
+ 
+  }
+]
+ 
 export default function Contact(){
   return(
     <div className="containerContact">
@@ -44,10 +56,38 @@ export default function Contact(){
             <div className="contactCard">
               <div className="contactAvatar">{std.id}</div>
               <div className="contactName">{std.name}</div>
+              <div>{std.presence}</div>
+              
             </div>
           )
         })
       }
+
+      {
+        console.log(calls)
+      }
+
+      <div className="call">
+        <div className="headerCall">
+          <div>Data</div>
+          <div>Nome</div>
+          <div>Presença</div>
+        </div>
+ 
+        
+        {
+          calls.map((call) => {
+            return(
+              <div className="bodyCall">
+                <div>{call.date}</div>
+                <div>{call.student }</div>
+                <div>{call.presence}</div>
+              </div>
+            )
+          })
+        }
+        
+      </div>
  
     </div>
   )
